fd8b5d264ba5df95b0c8d55e91f3b06924584929
krunal dodiya <kunal.dodiya1@gmail.com>
2021-03-23 08:42:48 +0530

    added default value to completed boolean

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [TodoModule, MongooseModule.forRoot('mongodb://localhost/chat')],
+  imports: [
+    TodoModule,
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: process.env.MONGO_URI,
+      }),
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { loadEnvironmentVariables } from './config';
 
 async function bootstrap() {
+  await loadEnvironmentVariables();
+
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+
+  app.enableCors();
 
   await app.listen(3000);
 }
